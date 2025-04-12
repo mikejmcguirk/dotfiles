0baa33e72adbd3c96edaca7d8c2327e5c50838a4
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-04-11 22:45:08 -0400

    updated maim script

--- .local/bin/maim-script.sh
+++ .local/bin/maim-script.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-output=$HOME/Pictures/screenshots/"$(date +%s_%h%d_%H:%M:%S).png"
+screenshots_dir="$HOME/Pictures/screenshots"
+[ ! -d "$screenshots_dir" ] && mkdir -p "$screenshots_dir"
+
+output=$screenshots_dir/$(date +%s_%h%d_%H:%M:%S)".png"
 
 menu() {
     echo "" | dmenu -p "How Long?" | xargs -I _ sleep "_"
@@ -12,5 +15,5 @@ case "$1" in
     "fulltime") menu && maim "$output" || exit ;;
     "selecttime") menu && maim -s "$output" || exit ;;
 esac
-
-notify-send "Maim" "Screenshot Taken"
+#
+# notify-send "Maim" "Screenshot Taken"
