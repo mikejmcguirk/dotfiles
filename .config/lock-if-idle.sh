47c5b3e1e397df447a23da469147934cf9dbf91c
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-04-13 13:32:50 -0400

    back to betterlockscreen

--- .config/lock-if-idle.sh
+++ .config/lock-if-idle.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if pgrep -x "slick-greeter" > /dev/null; then
-    exit 0
-fi
+# If using default Mint greeter
+# if pgrep -x "slick-greeter" > /dev/null; then
+#     exit 0
+# fi
 
 if ! playerctl status 2>/dev/null | grep -q "Playing"; then
-    dm-tool lock
+    # dm-tool lock
+    betterlockscreen --lock dim --display 1
 fi
