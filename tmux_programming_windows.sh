4296ff3455ce79c5adab9b8122747c355703394a
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-12 09:55:18 -0400

    added tmux_programming windows to config

---
+++ tmux_programming_windows.sh
@@ -0,0 +1,10 @@
+tmux new-window -n "obsidian" -c ~/obsidian/main
+tmux new-window -n "nvim" -c ~/.config/nvim
+# tmux new-window -n "programming" -c ~/programming
+# tmux new-window -n "YAPS" -c ~/programming/yaps/
+# tmux new-window -n "Rust Ref" -c ~/programming/learning/rust_reference/
+# tmux new-window -n "AoC" -c ~/programming/code_challenges/advent_of_code/
+# tmux new-window -n "nvim_plugin_dev" -c ~/programming/nvim_plugin_dev
+# tmux new-window -n "defaults" -c ~/default_programming_files
+
+tmux select-window -t 1
