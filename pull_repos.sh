1cc7a86e91de9d866dfe1c427dff26aa103cffa9
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-11-20 16:42:23 -0500

    add pull repos script

---
+++ pull_repos.sh
@@ -0,0 +1,12 @@
+cd ~/.config/nvim || exit
+git pull
+
+cd ~/.local/share/nvim/site/pack/dev-plugins/opt/nvim-qf-rancher || exit
+git pull
+
+cd ~/mint_install || exit
+git pull
+
+cd ~ || exit
+# TODO: This does not see my config alias
+# config pull
